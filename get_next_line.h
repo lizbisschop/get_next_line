dab012182c2669a1428b4ed1f8b0c1db4370d1d5
Liz Bisschop <lbisscho@f0r1s4.codam.nl>
2019-12-20 15:51:42 +0100

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lbisscho <lbisscho@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/20 13:39:46 by lbisscho       #+#    #+#                */
+/*   Updated: 2019/12/16 11:43:19 by lbisscho      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_strndup(char *str, size_t end);
+
+#endif
---
+++ main.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lbisscho <lbisscho@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/12/20 13:53:43 by lbisscho       #+#    #+#                */
+/*   Updated: 2019/12/20 15:26:15 by lbisscho      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int count;
+	int fd;
+	char *line;
+
+	count = 1; 
+	fd = open("text", O_RDONLY);
+	while (count > 0)
+	{
+		count = get_next_line(fd, &line);
+		printf("%d | %s\n", count, line);
+		free(line);
+	}
+	while (fd)
+	{
+		
+	}
+	// count = get_next_line(fd, &line);
+	// printf("%d | \n", count);
+	return (0);
+}
